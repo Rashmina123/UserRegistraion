cb765985932b6b2ace585c176aaf3d83aacb473f
rukhsar <12008137.gvp@gujaratvidyapith.org>
2022-12-17 23:53:42 +0530

    Add project files.

---
+++ UserRegistraion/Controllers/Account/RegistrationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserRegistraion.Models;
+
+namespace UserRegistraion.Controllers.Account
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            ViewBag.GenderList = GetGender();
+            return View();
+        }
+        public List<string> GetGender()
+        {
+            return new List<string>() { "Male", "Female" };
+        }
+    }
+}
---
+++ UserRegistraion/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistraion.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter User Password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "The Password should contain one 1 Uppercase, 1 Number, 1 Special Char, and 1 Lowercase Char), Min Length (8 Chars)")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Enter Confirm Password")]
+        [Compare("Password", ErrorMessage ="Password should be match with Confirm Password...")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Enter User Email")]
+        [RegularExpression("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",ErrorMessage ="PLease Enter Valid Email Address")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^[789]\d{9}$",ErrorMessage ="Enter Proper Contact No")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "Select User Gender")]
+        [BindProperty]
+        public string  Gender { get; set; }
+
+        //public string[] Genders = new[] { "Male", "Female", "Unspecified" };
+  
+    [Required(ErrorMessage = "PLease Accept Terms")]
+        public bool AcceptTerms { get; set; }
+        
+              
+    }
+}
